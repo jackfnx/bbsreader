c7652ce90e09a42078838207c85d2d0ebfceae16
sixue <sixue@ustc>
2018-09-22 03:49:35 +0800

    hehe

--- Windows/BBSReader/MainWindow.xaml.cs
+++ Windows/BBSReader/MainWindow.xaml.cs
@@ -1,22 +1,12 @@
-﻿using HtmlAgilityPack;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Net;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ForumFetcher
 {
@@ -30,28 +20,92 @@ namespace ForumFetcher
             InitializeComponent();
 
             threads = new ObservableCollection<object>();
-            //threads.Add(new { Title = "fdjsk", Author = "djdfksdj", Time = "fdsfdsafds", Url = "fdsjkj" });
             listView.DataContext = threads;
+
+            savePath = "E:/turboc/sis001/";
             loadThreads();
+            setCurrentKeyword(null);
+        }
+
+        private void setCurrentKeyword(string keyword)
+        {
+            currentKeyword = keyword;
+
+            threads.Clear();
+            if (keyword == null)
+            {
+                List<string> tags = new List<string>(saveData.tags.Keys);
+                tags.Sort((x, y) => {
+                    if (saveData.favorites.Contains(x) && !saveData.favorites.Contains(y))
+                    {
+                        return -1;
+                    }
+                    else if (!saveData.favorites.Contains(x) && saveData.favorites.Contains(y))
+                    {
+                        return 1;
+                    }
+                    return string.Compare(x, y);
+                });
+                tags.ForEach(x => threads.Add(new { Title = x, Author = "-", Time = "", Url = "", ThreadId = "", Favorite = saveData.favorites.Contains(x) }));
+            }
+            else
+            {
+                saveData.tags[keyword].ForEach(x => threads.Add(new { Title = x.title, Author = x.author, Time = x.postTime, Url = x.link, ThreadId = x.threadId, Favorite = false }));
+            }
         }
 
         private void loadThreads()
         {
-            string metaPath = "E:/turboc/sis/meta.json";
+            string metaPath = savePath + "meta.json";
             using (StreamReader sr = new StreamReader(metaPath, Encoding.UTF8))
             {
                 string json = sr.ReadToEnd();
-                List<BBSThread> l = JsonConvert.DeserializeObject<List<BBSThread>>(json);
-                l.ForEach(x => threads.Add(new { Title = x.title, Author = x.author, Time = x.postTime, Url = x.link }));
+                saveData = JsonConvert.DeserializeObject<SAVE_DATA>(json);
             }
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            //threads.Add(new { Title = "fdjsk", Author = "djdfksdj", Time = "fdsfdsafds", Url = "fdsjkj" });
+            setCurrentKeyword(null);
+        }
+
+        private void HandleDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            ListViewItem lvi = sender as ListViewItem;
+            dynamic item = lvi.Content;
+            if (currentKeyword == null)
+            {
+                setCurrentKeyword(item.Title);
+            }
+            else
+            {
+                string fPath = savePath + item.ThreadId + ".txt";
+                using (StreamReader sr = new StreamReader(fPath, Encoding.UTF8))
+                {
+                    string text = sr.ReadToEnd();
+                    Console.WriteLine(text);
+                }
+            }
         }
 
         public ObservableCollection<object> threads;
+        private string savePath;
+        private SAVE_DATA saveData;
+        private string currentKeyword;
+
+        public struct SAVE_DATA
+        {
+            [JsonProperty("timestamp")]
+            public long timestamp;
+            [JsonProperty("threads")]
+            public List<BBSThread> threads;
+            [JsonProperty("tags")]
+            public Dictionary<string, List<BBSThread>> tags;
+            [JsonProperty("favorites")]
+            public List<string> favorites;
+            [JsonProperty("blacklist")]
+            public List<string> blacklist;
+        }
 
         public struct BBSThread
         {
