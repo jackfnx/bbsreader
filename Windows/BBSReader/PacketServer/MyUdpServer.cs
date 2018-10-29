d39702134905123a855ce2d28f385bc655af6545
sixue <sixue@bbs.ustc.edu.cn>
2018-10-29 22:05:14 +0800

    完善udpserver

--- Windows/BBSReader/PacketServer/MyUdpServer.cs
+++ Windows/BBSReader/PacketServer/MyUdpServer.cs
@@ -35,7 +35,7 @@ namespace BBSReader.PacketServer
                         EndPoint remote = new IPEndPoint(IPAddress.Any, 0);
                         byte[] buffer = new byte[1024];
                         int length = udpListener.ReceiveFrom(buffer, ref remote);
-                        string message = Encoding.UTF8.GetString(buffer, 0, length);
+                        string message = Encoding.UTF8.GetString(buffer, 1, length - 1);
                         if (message == CODES_WORD)
                         {
                             return true;
@@ -74,7 +74,10 @@ namespace BBSReader.PacketServer
                     }
 
                     IPEndPoint iep = new IPEndPoint(IPAddress.Broadcast, port);
-                    byte[] buffer = Encoding.UTF8.GetBytes(CODES_WORD);
+                    byte[] bytes = Encoding.UTF8.GetBytes(CODES_WORD);
+                    byte[] buffer = new byte[bytes.Length + 1];
+                    buffer[0] = (byte)bytes.Length;
+                    bytes.CopyTo(buffer, 1);
                     broadcastSocket.SendTo(buffer, iep);
 
                     ServerStarted(this, EventArgs.Empty);
