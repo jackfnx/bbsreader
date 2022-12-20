48c6400bbd74d8ae94590ffc970c88c9206a701c
sixue <sixue@bbs.ustc.edu.cn>
2022-12-21 01:48:26 +0800

    req.txt

--- Python3/bbsreader_lib.py
+++ Python3/bbsreader_lib.py
@@ -92,7 +92,7 @@ class Crawler:
 
         self.proxy = {
             "http": "http://127.0.0.1:8080",
-            "https": "https://127.0.0.1:8080"
+            "https": "https://127.0.0.1:8080",
         }
 
         self.delay = delay
